6c5d7f4003c610f079a17c947b8607650f51ad95
Joey Dye <joey.m.dye@gmail.com>
2021-01-02 19:31:09 -0600

    fix: validation errors

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const { check } = require('express-validator/check');
-const userController = require('../controllers/userController');
+const express = require('express')
+const router = express.Router()
+const { check } = require('express-validator/check')
+const userController = require('../controllers/userController')
 
-router.post('/signin', userController.signIn);
+router.post('/signin', userController.signIn)
 router.post(
   '/register',
-  [
-    check('name').exists(),
-    check('email').isEmail(),
-    check('password').isLength({ min: 9 }),
-  ],
+  [check('name').exists(), check('email').isEmail()],
   userController.register
-);
+)
 
-module.exports = router;
+module.exports = router
