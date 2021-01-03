e18656e446d49051d33d6e92084940f7a4a1d456
Joey Dye <joey.m.dye@gmail.com>
2021-01-03 15:30:16 -0600

    feat: add dynamic port for heroku

--- start.js
+++ start.js
@@ -19,5 +19,5 @@ require('./models/User')
 
 // Start our app
 const app = require('./app')
-const port = 4000
+const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`App is listening on ${port}`))
