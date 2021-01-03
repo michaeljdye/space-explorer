8fb7e43ebc7e12e19b459bbd054548ce766b6295
Joey Dye <joey.m.dye@gmail.com>
2021-01-03 00:06:08 -0600

    redeploy

--- start.js
+++ start.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // import environmental variables from our .env file
-require('dotenv').config();
+require('dotenv').config()
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
-mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true })
+
+mongoose.set('useCreateIndex', true)
+
+mongoose.Promise = global.Promise // Tell Mongoose to use ES6 promises
+
 mongoose.connection.on('error', err => {
-  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-});
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`)
+})
 
 // Import all of our models
-require('./models/User');
+require('./models/User')
 
 // Start our app
-const app = require('./app');
-const port = 4000;
-app.listen(port, () => console.log(`App is listening on ${port}`));
+const app = require('./app')
+const port = 4000
+app.listen(port, () => console.log(`App is listening on ${port}`))
